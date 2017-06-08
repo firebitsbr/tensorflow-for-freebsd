5b4db35e99e3b4fbc46fe9a195f16d62bd33f9ff
User Akil <akil@wall.lab>
2017-06-08 11:44:36 +0000

    retrain custom data

---
+++ retrain_custom_data_photo.sh
@@ -0,0 +1,1 @@
+bazel-bin/tensorflow/examples/image_retraining/retrain --image_dir=/mnt/data/ai/inception-v4/data_photos --output_graph=/home/akil/workspace/ai/o1.pb --output_labels=/home/akil/workspace/ai/o1.txt
